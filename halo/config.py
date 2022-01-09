9894009345284395d2e00c2b773f8e7ec127a324
Khoa D. Vo <khoa.dubstep@gmail.com>
2022-01-09 08:10:27 +0700

    Add config

---
+++ halo/config.py
@@ -0,0 +1,22 @@
+import os
+
+ORIG_PATH = os.path.sep.join(["datasets", "original"])
+ORIG_IMAGES = os.path.sep.join([ORIG_PATH, "images"])
+ORIG_ANNOTS = os.path.sep.join([ORIG_PATH, "annotations"])
+
+GEN_PATH = os.path.sep.join(["datasets", "generated"])
+GEN_POSITIVE = os.path.sep.join([GEN_PATH, "raccoon"])
+GEN_NEGATIVE = os.path.sep.join([GEN_PATH, "no_raccoon"])
+
+MAX_PROPOSALS = 2000
+MAX_PROPOSALS_INFER = 200
+
+MAX_POSITIVE = 30
+MAX_NEGATIVE = 10
+
+INPUT_DIMS = (224, 224)
+
+MODEL_PATH = "raccoon_detector.h5"
+ENCODER_PATH = "raccoon_encoder.pickle"
+
+MIN_PROBA = 0.99
